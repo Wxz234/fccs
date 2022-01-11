c02eb5423bb25e00013b6cf69ced94d67eb2ea3b
Wxz234 <429373960@qq.com>
2022-01-11 18:29:41 +0800

    update commandlist

--- src/commandlist.cpp
+++ src/commandlist.cpp
@@ -2,7 +2,15 @@
 
 namespace fccs {
 	namespace rhi {
+		CommandList::CommandList(ID3D12Device* pDevice, QueueRef* pQueue, CommandListParameters parameters) {
+			m_device = pDevice;
+			m_queue = pQueue;
+			m_para = parameters;
+		}
 
+		void* CommandList::getNativePtr() const noexcept {
+			return nullptr;
+		}
 
 	}
 }
--- src/commandlist.h
+++ src/commandlist.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <fccs/fccs.h>
+#include "dx12_backend.h"
 namespace fccs {
 	namespace rhi {
 		class CommandList : public ICommandList {
 		public:
-		};
+			CommandList(ID3D12Device* pDevice, QueueRef* pQueue, CommandListParameters parameters);
+			void* getNativePtr() const noexcept;
 
+		};
+		Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+		QueueRef* m_queue;
+		CommandListParameters m_para;
+
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_MainList;
 	}
 }
