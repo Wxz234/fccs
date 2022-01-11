77ddf3cae583c8f092ba1db279a6cd5926942ff3
Wxz234 <429373960@qq.com>
2022-01-11 19:13:52 +0800

    update cmdlist

--- src/commandlist.h
+++ src/commandlist.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <fccs/fccs.h>
+#include <list>
 #include "dx12_backend.h"
 namespace fccs {
 	namespace rhi {
@@ -8,11 +9,19 @@ namespace fccs {
 			CommandList(ID3D12Device* pDevice, QueueRef* pQueue, CommandListParameters parameters);
 			void* getNativePtr() const noexcept;
 
+			void open();
+			void close();
+
+
+			std::shared_ptr<InternalCommandList> createInternalCommandList() const;
+
 		};
 		Microsoft::WRL::ComPtr<ID3D12Device> m_device;
 		QueueRef* m_queue;
 		CommandListParameters m_para;
 
 		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_MainList;
+		std::list<std::shared_ptr<InternalCommandList>> m_CommandListPool;
+		std::shared_ptr<InternalCommandList> m_ActiveCommandList;
 	}
 }
