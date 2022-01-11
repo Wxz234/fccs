d1a02bf05f4af3e495f17981b6904e590ddf4659
Wxz <429373960@qq.com>
2022-01-11 22:57:40 +0800

    add InternalCommandList

--- src/dx12_backend.h
+++ src/dx12_backend.h
@@ -13,12 +13,24 @@ namespace fccs {
 		Microsoft::WRL::ComPtr<ID3D12CommandQueue> queue;
 		Microsoft::WRL::ComPtr<ID3D12Fence> fence;
 
+		uint64_t lastSubmittedInstance = 0;
+		uint64_t lastCompletedInstance = 0;
+
 		QueueRef(ID3D12Device *pDevice,ID3D12CommandQueue *pQueue) {
 			queue = pQueue;
 			pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 		}
 
 
+	};
+
+	class InternalCommandList
+	{
+	public:
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> allocator;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList;
+
+		uint64_t lastSubmittedInstance = 0;
 	};
 
 }
