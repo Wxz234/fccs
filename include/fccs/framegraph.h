5115889398a876c6e624e6834bbf4df8ed1e9c47
Wxz234 <429373960@qq.com>
2022-01-19 14:50:25 +0800

    update fg

--- include/fccs/framegraph.h
+++ include/fccs/framegraph.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "core.h"
+#include "rhi.h"
+#include <string>
 namespace fccs {
 	namespace framegraph {
+		class Pass {
+		public:
+			Pass(rhi::IDevice* pDevice, std::string name) : m_Device(pDevice), m_passName(name) {}
+			virtual ~Pass(){}
+		private:
+			rhi::IDevice* m_Device;
+			std::string m_passName;
+		};
 
+		class FrameGraph {
+
+		};
 	}
 }
