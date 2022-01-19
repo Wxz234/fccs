7baf1b0051dbc217793b0d14cc24eba0d9d225ca
Wxz234 <429373960@qq.com>
2022-01-19 17:15:10 +0800

    add color class

--- include/fccs/core.h
+++ include/fccs/core.h
@@ -41,4 +41,17 @@ namespace fccs {
         virtual ~IResource();
     };
     FCCS_API void DestroyResource(IResource* pResource);
+
+    struct Color
+    {
+        float r, g, b, a;
+
+        Color() : r(0.f), g(0.f), b(0.f), a(0.f) { }
+        Color(float c) : r(c), g(c), b(c), a(c) { }
+        Color(float _r, float _g, float _b, float _a) : r(_r), g(_g), b(_b), a(_a) { }
+
+        bool operator ==(const Color& _b) const { return r == _b.r && g == _b.g && b == _b.b && a == _b.a; }
+        bool operator !=(const Color& _b) const { return !(*this == _b); }
+    };
+
 }
