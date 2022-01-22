5adfa09ca45b293efcbca77a798b8d4e709ae74f
Wxz <429373960@qq.com>
2022-01-22 13:53:30 +0800

    update texture.h

--- src/texture.h
+++ src/texture.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <fccs/rhi.h>
 #include <wrl/client.h>
+#include <vector>
 namespace fccs {
 	namespace rhi {
 		class Texture : public ITexture {
@@ -8,11 +9,29 @@ namespace fccs {
 			Texture(ID3D12Resource* resource, const TextureDesc& desc) :m_res(resource),m_desc(desc) {
 				
 			}
-
+			~Texture() {
+				for (auto heap : m_srv) {
+					heap->Release();
+				}
+				for (auto heap : m_uav) {
+					heap->Release();
+				}
+				for (auto heap : m_rtv) {
+					heap->Release();
+				}
+				for (auto heap : m_dsv) {
+					heap->Release();
+				}
+			}
 			void* GetNativePtr() const noexcept {
 				return m_res.Get();
 			}
+			Microsoft::WRL::ComPtr<ID3D12Device> m_device;
 			Microsoft::WRL::ComPtr<ID3D12Resource> m_res;
+			std::vector<ID3D12DescriptorHeap*> m_srv;
+			std::vector<ID3D12DescriptorHeap*> m_uav;
+			std::vector<ID3D12DescriptorHeap*> m_rtv;
+			std::vector<ID3D12DescriptorHeap*> m_dsv;
 			TextureDesc m_desc;
 		};
 	}
